c60d1762f7837749a2afd62cbaf9b048d69c5f71
Onto241 <dovidavicius.marius@gmail.com>
2017-06-28 20:35:16 +0100

    Update aplication.js

--- aplication.js
+++ aplication.js
@@ -37,6 +37,6 @@ var duplicate = function (array) {
 };
 
 var evaluateExpression = function (a, b, c) {
-    var expression = a + b + "("+ c +")";
+    var expression = a + c + "("+ b +")";
     return eval(expression);
 };
